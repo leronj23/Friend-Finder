f1f24baf08811442c1abf12c90ef6e404f55b26f
LeRon Jackson <leronj23@gmail.com>
2019-02-23 16:03:19 -0500

    update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
     });
 
     // default to home
-    app.get("*", function (req, res) {
+    app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
